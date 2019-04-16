fcdcaea81f5b512c51ba832f9ce04f0d5f800784
ViacheslavP <public.viacheslav@gmail.com>
2019-04-16 17:10:10 +0300

    1

--- core/one_D_scattering.py
+++ core/one_D_scattering.py
@@ -1444,7 +1444,7 @@ if __name__ == '__main__':
     #rc('text', usetex=True)
 
     args = {
-            'nat':500, #number of atoms
+            'nat':10, #number of atoms
             'nb':0, #number of neighbours in raman chanel (for L-atom only)
             's':'nocorrchain', #Stands for atom positioning : chain, nocorrchain and doublechain
             'dist':0.,  # sigma for displacement (choose 'chain' for gauss displacement., \lambda/2 units)
--- core/wrap_bypass.py
+++ core/wrap_bypass.py
@@ -3,10 +3,12 @@ def get_solution(dim, nof, noa, Sigma, ddRight, freq, gamma, rabi, dc):
     import ctypes
     import numpy as np
 
-    calcalib = ctypes.cdll.LoadLibrary('/home/viacheslav/Projects/PycharmProjects/calculations/core/libbypass.so')
-    scV = np.empty(dim*nof, dtype=np.complex64)
+    try:
+        raise TypeError
+        calcalib = ctypes.cdll.LoadLibrary('/home/viacheslav/Projects/PycharmProjects/calculations/core/libbypass.so')
+        scV = np.empty(dim*nof, dtype=np.complex64)
 
-    calcalib.scProblem_solve.argtypes = [ctypes.c_int,
+        calcalib.scProblem_solve.argtypes = [ctypes.c_int,
                                          ctypes.c_int,
                                          ctypes.c_int,
                                          np.ctypeslib.ndpointer(np.complex64, ndim=1, flags='C'),
@@ -18,7 +20,7 @@ def get_solution(dim, nof, noa, Sigma, ddRight, freq, gamma, rabi, dc):
                                          np.ctypeslib.ndpointer(np.complex64, ndim=1, flags='C')]
 
 
-    calcalib.scProblem_solve(dim,
+        calcalib.scProblem_solve(dim,
                              nof,
                              noa,
                              np.reshape(np.eye(dim)+Sigma, (dim*dim)).astype(np.complex64),
@@ -29,4 +31,12 @@ def get_solution(dim, nof, noa, Sigma, ddRight, freq, gamma, rabi, dc):
                              dc,
                              scV)
 
-    return np.transpose(np.reshape(scV, (nof, dim)))
+        return np.transpose(np.reshape(scV, (nof, dim)))
+
+    except:
+
+        scV = np.ones((dim,nof), dtype=np.complex64)
+        for i,om in enumerate(freq):
+            resolvent = np.eye(dim)*(-om + rabi[0]**2 / (4*(om - dc)) - 0.5j*gamma) + Sigma
+            scV[:,i] = np.linalg.solve(resolvent, ddRight)
+        return scV
